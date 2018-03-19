4c5d4edc03e7301917a602fa73e7288133c54ccc
Kyle Beyer <beykyle@umich.edu>
2018-03-19 16:02:20 -0400

    fixed boundary condition inputs

--- Slabby.py
+++ Slabby.py
@@ -19,21 +19,8 @@ __email__ = "beykyle@umich.edu"
 __status__ = "Development"
 
 class Slab:
-  def __init__(self , numBins , width , *args , **kwargs):
+  def __init__(self ,  *args , **kwargs):
     print("Intializing slab!")
-    # initialize varaiables
-    self.numBins     = int(numBins)
-    self.binWidth    = float(width / numBins)
-    self.width      =  float(width)
-    self.Q           = np.zeros(numBins)
-    self.SigT        = np.zeros(numBins)
-    self.SigS        = np.zeros(numBins)
-    self.currentEps  = 1000 # a big number
-    self.rho         = 0
-
-    print("number of bins: " + str(numBins))
-    print("slab width : "    + str(width))
-
     # get key word constructor arguments
     if kwargs is not None:
       for key, value in kwargs.items():
@@ -53,37 +40,67 @@ class Slab:
           print(key + ": " + str(value))
           setattr(self , key , value)
 
-
-    # inital scalar flux guess
-    self.scalarFlux = np.ones(numBins)
+    self.currentEps  = 1000 # a big number
+    self.rho         = 0
 
     # initialize quad set weights ,  make sure they're normalized
     self.mu , self.weights = roots_legendre(self.quadSetOrder , mu=False)
     self.weights = np.array(self.weights)
     self.weights = self.weights / sum(self.weights)
 
-  def setHomogenousData(self , sigt , sigs , q):
-   for i in range(0,self.numBins-1):
-     self.Q[i]    = q
-     self.SigS[i] = sigs
-     self.SigT[i] = sigt
+  def setHomogenousData(self , numBins , width , sigt , sigs , q):
+    self.numBins     = int(numBins)
+    self.binWidth    = float(width / numBins)
+    self.width      =  float(width)
+    self.Q           = np.zeros(numBins)
+    self.SigT        = np.zeros(numBins)
+    self.SigS        = np.zeros(numBins)
+    for i in range(0,self.numBins-1):
+      self.Q[i]    = q
+      self.SigS[i] = sigs
+      self.SigT[i] = sigt
+    print("number of bins: " + str(numBins))
+    print("slab width : "    + str(width))
+
+
 
   def getMatDataFromFile(self , filename):
-   # alternative to homogenous data
-   raise NotImplementedError
+    with open(filename, "r") as dat:
+      headers = dat.readline().split(",")
+      for i , header in enumerate(headers):
+        if header.strip().rstrip("\r\n") == "z":
+          zInd = i
+        elif header.strip().rstrip("\r\n") == "SigS":
+          sInd = i
+        elif header.strip().rstrip("\r\n") == "SigT":
+          tInd = i
+        elif header.strip().rstrip("\r\n") == "Q":
+          qInd = i
+
+      data = dat.readlines()[1:]
+
+    self.binWidth = float(data[1][zInd].strip().rstrip("\r\n")) - float(data[0][zInd].strip().rstrip("\r\n"))
+    self.width    = float(data[-1][zInd].strip().rstrip("\r\n"))
+    self.numBins  = len(data[zInd][:])
+    self.Q    = np.array([float(x) for x in data[:][qInd].strip().rstrip("\r\n") ])
+    self.SigT = np.array([float(x) for x in data[:][tInd].strip().rstrip("\r\n") ])
+    self.SigS = np.array([float(x) for x in data[:][sInd].strip().rstrip("\r\n") ])
+
+    print("number of bins: " + str(numBins))
+    print("slab width : "    + str(width))
 
   def writeOutput(self , filename):
     # write the diagnostics to the output file
     with open(filename , "a") as output:
-      output.write("Epsilon: " + '{:1.8f}'.format(self.currentEps) + " , Rho: " + '{:1.8f}'.format(self.rho) + "\r\n")
+      output.write("Epsilon: " + '{:1.9E}'.format(self.currentEps) + " , Rho: " + '{:1.9E}'.format(self.rho) + "\r\n")
     # print the diagnostics to the command line
-    print("Epsilon: " + '{:02.4f}'.format(self.currentEps) + " , Rho: " + '{:02.4f}'.format(self.rho) + "\r\n")
+    print("Epsilon: " + '{:1.9E}'.format(self.currentEps) + " , Rho: " + '{:1.9E}'.format(self.rho) + "\r\n")
 
   def plotScalarFlux(self, iterNum ):
     plt.ion()
     plt.cla()
     x = np.linspace(0 , self.width , self.numBins)
-    plt.plot(x , self.scalarFlux , "k*")
+    plt.plot(x , self.scalarFlux , "k.")
     plt.xlabel(r"$x$ [cm]")
     plt.ylabel(r"scalar flux, $\Phi$ [cm$^{-2}$ s$^{-1}$ ]")
     plt.title("Iteration " + str(iterNum))
@@ -112,10 +129,20 @@ class Slab:
       for key, value in kwargs.items():
         if key == "boundaryType":
           setattr(self , "rightBoundaryType" , value)
-    self.rightBoundaryFlux = np.ones(int(self.quadSetOrder / 2)) * rightFlux
-    if self.rightBoundaryType == "Isotropic":
-      for i , val in self.rightBoundaryFlux:
+
+    self.rightBoundaryFlux = np.zeros( int(self.quadSetOrder / 2)) # handled at sweep time
+    if self.rightBoundaryType   == "planar":
+      self.rightBoundaryFlux[-1] =  rightFlux
+    elif self.rightBoundaryType == "isotropic":
+      for i , val in enumerate(self.rightBoundaryFlux):
         self.rightBoundaryFlux[i] = rightFlux / (4 * np.pi) * self.weights[i]
+    elif self.rightBoundaryType == "vacuum":
+      pass
+    elif self.rightBoundaryType == "reflecting":
+      pass
+    else:
+      print("Invalid boundary type: " + self.leftBoundaryType + " for left boundary! \r\n")
+      sys.exit()
 
   def setLeftBoundaryFlux(self , leftFlux , *args , **kwargs):
     self.leftFlux = leftFlux
@@ -123,10 +150,18 @@ class Slab:
       for key, value in kwargs.items():
         if key == "boundaryType":
           setattr(self , "leftBoundaryType" , value)
-    self.leftBoundaryFlux = np.ones( int(self.quadSetOrder / 2)) * leftFlux
-    if self.leftBoundaryType == "Isotropic":
-      for i , val in self.leftBoundaryFlux:
+
+    self.leftBoundaryFlux = np.zeros(int(self.quadSetOrder / 2))
+    if self.leftBoundaryType   == "planar":
+      self.leftBoundaryFlux[0] = leftFlux # set mu=0 direction to the given flux
+    elif self.leftBoundaryType == "isotropic":
+      for i , val in enumerate(self.leftBoundaryFlux):
         self.leftBoundaryFlux[i] = leftFlux / (4 * np.pi) * self.weights[i]
+    elif self.leftBoundaryType == "vacuum":
+      pass
+    else:
+      print("Invalid boundary type: " + self.leftBoundaryType + " for left boundary! \r\n")
+      sys.exit()
 
   def getScalarFlux(self , psi , *args , **kwargs):
     # integrate the angular flux in a spatial bin according to the Gauss-Legendre quad sets
@@ -192,16 +227,18 @@ class Slab:
       psiIn = psiOut
 
   def estimateRho(self , oldScalarFlux):
-    self.rho = np.dot(self.scalarFlux , self.scalarFlux) / np.dot(oldScalarFlux , oldScalarFlux)
+    self.rho = np.dot(self.scalarFlux , self.scalarFlux) / (0.000000001 + np.dot(oldScalarFlux , oldScalarFlux))
 
   def testConvergence(self , oldScalarFlux):
     self.currentEps = max( np.divide( np.abs(self.scalarFlux - oldScalarFlux)  ,  np.abs(self.scalarFlux) + 0.000001 ) )
 
   def clearOutput(self):
     with open(self.out , "w") as outt:
-      outt.write("Running 1-D transport!")
+      outt.write("Running 1-D transport! \r\n")
 
   def run(self):
+    # inital scalar flux guess
+    self.scalarFlux = np.zeros(self.numBins)
     iterationNum = 0
     self.clearOutput()
 
@@ -222,6 +259,7 @@ class Slab:
       self.c2rl[i,:] = (np.abs(self.mu[:N]) + (1 + np.abs(self.alpha)) * self.SigT[i] * self.binWidth / 2)[:]
 
     while(self.currentEps > self.epsilon):
+      self.plotScalarFlux(iterationNum)
       iterationNum += 1
       # run a transport sweep
       oldScalarFlux = np.copy( self.scalarFlux[:] )
@@ -242,9 +280,9 @@ class Slab:
 
     # the simulation is done, write the scalar flux to the output file
     with open(self.out , "a") as output:
-      output.write("\r\n Scalar Flux: \r\n")
+      output.write("\r\n x , Scalar Flux: \r\n")
       for i , val in enumerate(self.scalarFlux):
-        output.write('{:1.4f}'.format( i * self.width / self.numBins ) + "   " + '{:1.7f}'.format(val) )
+        output.write('{:1.4f}'.format( i * self.width / self.numBins ) + " , " + '{:1.7f}'.format(val) + "\r\n")
 
 
 def booleanize(value):
@@ -282,17 +320,18 @@ if __name__ == '__main__':
     sys.exit()
 
   if 'Slab' in conf:
-    if 'matData' in conf['Slab']:
-      print("non-homogenous data not yet implemented!")
-      raise NotImplementedError
-      sys.exit()
-    else:
-      homogenous    =  booleanize( conf['Slab']['homogenous'].strip() )
+    homogenous    =  booleanize( conf['Slab']['homogenous'].strip() )
+    if 'matData' in conf['Slab'] and homogenous == False:
+      matData = conf['Slab']['matData']
+    elif homogenous == True:
       bins          =  int(        conf['Slab']['bins'].strip()       )
       width         =  float(      conf['Slab']['width'].strip()      )
       SigT          =  float(      conf['Slab']['SigT'].strip()       )
       SigS          =  float(      conf['Slab']['SigS'].strip()       )
       Q             =  float(      conf['Slab']['Q'].strip()          )
+    else:
+      print("mat data not inputted!")
+      sys.exit()
   else:
     raise
     print("No Slab section found in input file! Exiting! ")
@@ -314,14 +353,14 @@ if __name__ == '__main__':
 
   # Create slab object and run the simulation
   # hardcoded values: 100 maximum iterations, alpha=0 for diamond difference
-  slab = Slab(bins , width , loud=loud , diagnostic=diagnostic , quadSetOrder=quadSetOrder , epsilon=epsilon , out=outputFi , maxIter=100 , alpha=0)
+  slab = Slab(loud=loud , diagnostic=diagnostic , quadSetOrder=quadSetOrder , epsilon=epsilon , out=outputFi , maxIter=100 , alpha=0)
   if homogenous == True:
-    slab.setHomogenousData(SigT , SigS , Q)
+    slab.setHomogenousData(bins , width , SigT , SigS , Q)
   else:
-    raise NotImplementedError
+    slab.getMatDataFromFile(matData)
 
-  slab.setRightBoundaryFlux( rightFlux , boundaryType = right)
-  slab.setLeftBoundaryFlux(  leftFlux , boundaryType = left )
+  slab.setRightBoundaryFlux( rightFlux , boundaryType=right)
+  slab.setLeftBoundaryFlux(  leftFlux  , boundaryType=left )
   slab.run()
 
 
